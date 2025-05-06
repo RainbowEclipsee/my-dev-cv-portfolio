b81f9e8b93e74913a82aa047d7aad9e1b36283c6
RainbowEclipsee <dexterrullez1@gmail.com>
2025-05-06 16:23:34 +0300

    Contacts page - added icons

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -7,14 +7,26 @@ const Contacts: React.FC = () => {
         <ul className="content-list">
           <li className="content-list__item">
             <h2 className="title-2">Location</h2>
-            <p>Saint Petersburg, Russia</p>
+            <p style={{ display: 'inline-flex', alignItems: 'center' }}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                style={{ marginRight: '8px' }}
+              >
+                <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5s2.5 1.12 2.5 2.5S13.38 11.5 12 11.5z" />
+              </svg>
+              Saint Petersburg, Russia
+            </p>
           </li>
           <li className="content-list__item">
             <h2 className="title-2">Telegram</h2>
             <p>
               <a
                 href="https://t.me/makarchuk_bogdan"
-                target="_blank"
+                target="blank"
                 style={{
                   display: 'inline-flex',
                   alignItems: 'center',
@@ -22,7 +34,6 @@ const Contacts: React.FC = () => {
                   color: 'inherit',
                 }}
               >
-                {' '}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
@@ -40,7 +51,25 @@ const Contacts: React.FC = () => {
           <li className="content-list__item">
             <h2 className="title-2">Email</h2>
             <p>
-              <a href="mailto:makarchuk-bogdan@yandex.ru" style={{color: 'inherit'}}>
+              <a
+                href="mailto:makarchuk-bogdan@yandex.ru"
+                style={{
+                  display: 'inline-flex',
+                  alignItems: 'center',
+                  textDecoration: 'none',
+                  color: 'inherit',
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  style={{ marginRight: '8px' }}
+                >
+                  <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z" />
+                </svg>
                 makarchuk-bogdan@yandex.ru
               </a>
             </p>
